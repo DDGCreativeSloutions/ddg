129b753e5606157c639b44a831885e448702a422
syedalthaf786 <syedalthaf760.com>
2025-06-14 17:33:56 +0530

    size increase

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -228,8 +228,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 >
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="12"
-    height="12"
+    width="32"
+    height="32"
     fill="white"
     viewBox="0 0 24 24"
   >
